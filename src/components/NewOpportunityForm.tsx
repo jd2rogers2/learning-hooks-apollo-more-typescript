f861c10e25e720f0be2ed22756967010a56c3908
jd2rogers2 <jd2rogers2@gmail.com>
2019-07-31 14:56:35 -0400

    form done, FE done more or less minus apollo and styling

--- src/components/NewOpportunityForm.tsx
+++ src/components/NewOpportunityForm.tsx
@@ -7,33 +7,24 @@ export const NewOpportunityForm: React.FC<INewOpportunityFormProps> = ({addOppor
     location: ''
   });
 
-  const handleSubmit = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    // if (!input) return;
-    addOpportunity({
-      title: input,
-      location: '',
-      user_id: '1',
-      description: '',
-      id: ''
-    });
-    setInput({title: '', description: '', location: ''});
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput({...input, [name]: value});
   }
 
-  const handleInputChange = e => {
-    // setInput(e.target.value);
-  }
-
-  const buildOpp = () => {}
+  const buildOpp = () => ({ title: input.title, description: input.description, location: input.location, user_id: '1', id: ''})
 
   return (
     <div>
       <label>Title: </label>
-      <input type="text" name="title" value={input.title} onChange={handleInputChange} placehold="add a title..." />
+      <input type="text" name="title" value={input.title} onChange={handleInputChange} placeholder="add a title..." />
+      <br />
       <label>Description: </label>
-      <input type="text" name="description" value={input.description} onChange={handleInputChange} placehold="add a description..." />
+      <input type="text" name="description" value={input.description} onChange={handleInputChange} placeholder="add a description..." />
+      <br />
       <label>Location: </label>
-      <input type="text" name="location" value={input.location} onChange={handleInputChange} placehold="add a location..." />
+      <input type="text" name="location" value={input.location} onChange={handleInputChange} placeholder="add a location..." />
+      <br />
       <input type="submit" value="Create new opportunity" onClick={() => addOpportunity(buildOpp())} />
     </div>
   );
--- src/components/Opportunity.tsx
+++ src/components/Opportunity.tsx
@@ -3,7 +3,9 @@ import * as React from 'react';
 export const Opportunity: React.FC<IOpportunityProps> = ({opportunity}) => {
   return opportunity ? (
     <div>
-      {opportunity.title}
+      <h5>{opportunity.title}</h5>
+      <p>{opportunity.location}</p>
+      <p>{opportunity.description}</p>
     </div>
   ) : (
     <div></div>
